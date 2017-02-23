71057bdd1bf1a6a4b11fbb567582d4f7351f6de4
zenjack <zenjack@foxmail.com>
2017-02-24 00:55:24 +0800

    添加进入不同组件，使用不同进入过渡。

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
+    <transition :name="transitionName">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      transitionName: ''
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      const toPath = to.path
+      this.transitionName = toPath.slice(1)
+    }
+  }
 }
 </script>
 
@@ -17,4 +30,27 @@ export default {
 
   #app
     font-size: 16px
+  
+  .search-enter-active
+    transition: all 0.2s linear
+    transform: scale(1, 1)
+    opacity: 1
+  .search-enter
+    transform: scale(0.5, 0.5)
+    opacity: 0.8
+  // .me-enter-active
+  //   transition: all 0.2s linear
+  //   transform: scale(1, 1)
+  // .me-enter
+  //   transform: scale(0, 1)
+  .discover-enter-active
+    transition: all 0.2s linear
+    transform: scale(1, 1)
+  .discover-enter
+    transform: scale(0, 1)
+  // .contact-enter-active
+  //   transition: all 0.5s linear
+  //   transform: translate(0, 0)
+  // .contact-enter
+  //   transform: translate(100%, -100%)
 </style>
